3849c6d47591c1fddd56ce00a5e697387d4a5f28
Abanob <abanobm94@gmail.com>
2023-10-17 18:26:10 -0700

    Abanob: Edited shell.c

--- shell.c
+++ shell.c
@@ -1,8 +1,26 @@
 #include "libraries.h"
 
-nt main(int ac, char **av, char **envp)
-{
-    
+/**
+ * main - shell copy program
+ *
+ * @ac: number of arguments
+ * @av: arguments passed to the program
+ * @envp: environment variable for paths later
+ *
+ * Return: 0
+*/
 
-    return (0);
+int main(int ac, char **av, char **envp)
+{
+	(void)ac;
+	int j;
+	char command[], paras[];
+
+	while (1)
+	{
+	printf("$ ");
+
+	}
+
+	return (0)
 }
