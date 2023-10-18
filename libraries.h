935cee0d68945b9b188415e28dfd45048c526b2f
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-10-18 04:17:01 +0300

    Amr: made some changes for libraries.h

--- libraries.h
+++ libraries.h
@@ -1,15 +1,24 @@
 #ifndef LIBRARIES_H
 #define LIBRARIES_H
+
+/* Required Libraries for Simple Shell */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+
+/* Prototypes used for Shell */
+
 #include "functions.c"
 
-void getline();
-void shellexecute();
+/*void shell_exit(char *command, char *parameters[]);*/
+void __shell_getline(char *cmd, char **par);
+void shell_execute(char *cmd, char *command,
+	char **parameters, char **envp, char **av);
+
+/* Variable Limitations */
 
 #define MAX_LENGTH_OF_CMD 1024
 #define MAX_LENGTH_OF_PARAMETERS 64
